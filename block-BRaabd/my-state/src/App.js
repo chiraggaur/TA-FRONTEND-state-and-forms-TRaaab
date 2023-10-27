1aa24e9a8dd7c2528be8175c8ed13bfd771d43f0
chirag gaur <chiragwalker@gmail.com>
2023-10-27 15:07:48 +0530

    done assignment buttons/images render with state

--- block-BRaabd/my-state/src/App.js
+++ block-BRaabd/my-state/src/App.js
@@ -1,6 +1,42 @@
 import React from "react";
-import Button from "./button";
-function App() {
-  return <Button name="basketball" />;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Active: "basketball",
+    };
+  }
+  render() {
+    let data = ["basketball", "cricket", "laptop", "phone", "pubg", "tiger"];
+    return (
+      <>
+        <div className="Button_Container">
+          {data.map((elm) => {
+            return (
+              <button
+                key={elm}
+                className={this.state.Active === elm ? "active" : "button"}
+                onClick={() => {
+                  this.setState({
+                    Active: elm,
+                  });
+                }}
+              >
+                {elm}
+              </button>
+            );
+          })}
+        </div>
+        <div className="Image_container">
+          <img
+            src={`./images/${this.state.Active}.jpg`}
+            className="Images"
+            alt={this.state.Active}
+          />
+        </div>
+      </>
+    );
+  }
 }
 export default App;
